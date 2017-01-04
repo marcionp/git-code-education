0930b323b1f778c7631edaaf7d5234b29a54e6d0
marcio <marcionp@gmail.com>
2017-01-04 19:43:03 -0200

    vue.js - Exercicio Manipulando estruturas básicas

--- laravel-vue/vue.js/app.js
+++ laravel-vue/vue.js/app.js
@@ -4,7 +4,26 @@
 var app = new Vue({
     el: "#app",
     data: {
+        test: "teste",
         title: "Contas a pagar",
+        menus: [
+            {id: 0, name: "Listar contas"},
+            {id: 1, name: "Criar conta"}
+        ],
+        activedView: 1,
+        formType: 'insert',
+        bill: {
+            date_due: '',
+            name: '',
+            value: 0,
+            done: 0
+        },
+        names: [
+            'Conta de Luz',
+            'Internet',
+            'Cartão',
+            'Combustivel'
+        ],
         bills: [
             {date_due: '01/01/2017', name: 'Conta de Luz', value: 98.00, done: 1},
             {date_due: '01/01/2017', name: 'Internet', value: 98.00, done: 0},
@@ -13,21 +32,87 @@ var app = new Vue({
         ]
     },
     computed: {
-        status: function () {
-            var count = 0;
+        countBill: function () {
+            return this.bills.length;
+            },
+        countPendingBill: function () {
+            var count = -1;
             for (var i in this.bills) {
+                count = 0;
                 if (!this.bills[i].done) {
                     count++;
                 }
             }
-            return !count ? "Nenhuma conta a pagar" : "Existem " + count + " contas em aberto";
+            return count;
+        }
+    },
+
+    methods: {
+        showView: function (menuId) {
+            this.activedView = menuId;
+            if (menuId == 1){
+                this.formType = 'insert';
+            }
+            else
+            {
+                this.formType = 'update';
+            }
+        },
+        submit: function(){
+            if (this.formType == 'insert') {
+                this.bills.push(this.bill);
+            }
+            this.bill = {
+                date_due: '',
+                name: '',
+                value: 0,
+                done: 0
+            }
+            this.activedView = 0;
+        },
+        loadBill: function (bill) {
+            this.bill = bill;
+            this.activedView = 1;
+            this.formType = 'update';
+        },
+        writeOffBill: function (bill) {
+            if (confirm("Confirma pagamento?")) {
+                bill.done = 1;
+            }
+        },
+        killBill: function (bill) {
+            if (confirm("Confirma exclusão de conta " + bill.name + " no valor de " + bill.value + "?")) {
+                var pos = this.bills.indexOf(bill);
+
+                this.bills.splice(pos,1);
+            }
         }
     }
-})
+});
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date()
+Vue.filter('doneLabel', function (a) {
+   if (a == "0") {
+       return "Em Aberto";
+   }
+   else {
+       return "Paga";
+   }
+});
+
+Vue.filter('reportStatus', function (a) {
+    if (a < 0) {
+        return "Nenhuma conta cadastrada";
     }
-})
+    else{
+        if (a > 0){
+            return "Existem " + a + " contas pendentes";
+        }
+        else {
+            return "Nenhuma conta a pagar";
+        }
+    }
+
+});
+
+
+
