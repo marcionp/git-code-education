c257bc65c97d76f0fece3f72ea78af45f9213e91
marcio <marcionp@gmail.com>
2017-02-14 19:48:50 -0200

    revisao vue

---
+++ laravel-vue/vue-rev/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Contas a pagar",
+        bills: [
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50}
+            ]
+    }
+})
