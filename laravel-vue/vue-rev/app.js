187c196c738105a3bc01fd904b5eeeb30443b381
marcio <marcionp@gmail.com>
2017-02-14 20:02:34 -0200

    vue-listagem_dados

--- laravel-vue/vue-rev/app.js
+++ laravel-vue/vue-rev/app.js
@@ -4,6 +4,8 @@ var app = new Vue({
         title: "Contas a pagar",
         bills: [
             {date_due: '01/01/2016', name: 'Luz', value: 100.50},
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
             {date_due: '01/01/2016', name: 'Luz', value: 100.50}
             ]
     }
