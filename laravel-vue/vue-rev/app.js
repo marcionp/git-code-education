5de0a2c10bf9efe7417af424d107c6cb22d2096d
marcio <marcionp@gmail.com>
2017-02-14 20:20:42 -0200

    vue-Propriedades computadas

--- laravel-vue/vue-rev/app.js
+++ laravel-vue/vue-rev/app.js
@@ -3,10 +3,21 @@ var app = new Vue({
     data: {
         title: "Contas a pagar",
         bills: [
-            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
-            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
-            {date_due: '01/01/2016', name: 'Luz', value: 100.50},
-            {date_due: '01/01/2016', name: 'Luz', value: 100.50}
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50, done: 1},
+            {date_due: '01/01/2016', name: 'Luz', value: 200.50, done: 0},
+            {date_due: '01/01/2016', name: 'Luz', value: 100.50, done: 0},
+            {date_due: '01/01/2016', name: 'Luz', value: 40.50,done: 1 }
             ]
+    },
+    computed: {
+        status: function () {
+            var count = 0;
+            for (var i in this.bills) {
+                if (!this.bills[i].done) {
+                    count++;
+                }
+            }
+            return !count ? "Nenhuma conta a pagar" : "Existem " + count + " contas a pagar";
+        }
     }
 })
